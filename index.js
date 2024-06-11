9ca63ac7c1f873e7d458512921d3d551816d769e
Hasan Gökgöz <91029442+hasangkz@users.noreply.github.com>
2024-06-11 17:22:02 +0300

    bug fix for auth middleware

--- index.js
+++ index.js
@@ -6,14 +6,31 @@ const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/userRoutes.js');
 const postRoutes = require('./routes/postRoutes.js');
 
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+//middl
+
 dotenv.config();
+const PORT = process.env.PORT || 5001;
+const app = express();
 connect();
 
-const app = express();
-const PORT = process.env.PORT || 5001;
-
 //middlewares
-app.use(cors());
+// app.use((req, res, next) => {
+//   res.header(
+//     'Access-Control-Allow-Origin',
+//     'https://fbucks-frontend.onrender.com'
+//   );
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
