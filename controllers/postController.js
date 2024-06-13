8986de2331f4c940f8b85a3701ad3b3329ee99ab
Hasan Gökgöz <91029442+hasangkz@users.noreply.github.com>
2024-06-13 17:54:23 +0300

    getuserspost function has been added

--- controllers/postController.js
+++ controllers/postController.js
@@ -123,7 +123,19 @@ const unlikePost = async (req, res) => {
 
 // GET USER'S POST
 const getUserPosts = async (req, res) => {
+  const { username } = req.params;
+
   try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const posts = await Post.find({ postedBy: user._id }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json(posts);
   } catch (error) {
     res.status(500).json({ error: error.message });
     console.log('Error in getUserPosts => ', error);
